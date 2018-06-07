51c07221a8be3fac4bf84127bcf40d6f57a287d5
Mairis Januns <mairisja@administrators-MacBook-Pro-9.local>
2018-06-07 18:24:41 +0300

    Changed DB_CONFIG

--- db_config.php
+++ db_config.php
@@ -1,28 +1,57 @@
 <!-- __________________________________________CONNECTION TO SQL __________________________________________________________________________ -->
 
+
 <?php
+// we get submitted data and check if user really submitted it
+if(isset($_GET['first_name']){
+$user = $_GET['first_name'];
 
-if(isset($_POST['first_name','last_name','email','password','password_confirmation'])){
-	$vards = $_POST['first_name'],
-	$uzvards = $_POST['last_name'],
-	$epasts = $_POST['email'],
-	$parole = $_POST['password'];
+// make an mysql connection, make sure to edit this
+mysql_connect("localhost", "root", "GT5364HY"); // host = host name, user = database user, password = database password
+mysql_select_db("rdk_db"); // database = mysql database name
 
+// escape input from dangerous charachters
+$user = htmlentities(mysql_real_escape_string($user));
 
-  $servername = "localhost";
-  $username = "root";
-  $password = "GT5364HY";
-  $dbname = "rdk_db";
+mysql_query("INSERT INTO users(vards) VALUES('$first_name')");
+echo ("Submitted."); 
 
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-
-  $user = htmlentities(mysql_real_escape_string($vards));
-  mysql_query(INSERT INTO users(vards) VALUES('$vards') echo "Submitted.");
-  $user = htmlentities(mysql_real_escape_string($uzvards));
-  mysql_query(INSERT INTO users(uzvards) VALUES('$uzvards') echo "Submitted.");
-  $user = htmlentities(mysql_real_escape_string($vards));
-  mysql_query(INSERT INTO users(epasts) VALUES('$epasts') echo "Submitted.");
+}
+else{
+// else print "you have not submitted anything
+die("You have not submitted anything.");
 }
 ?>
 
+
+
+
+
+
+
+// <!-- <?php
+
+// if(isset($_POST['first_name','last_name','email','password','password_confirmation'])){
+	// $vards = $_POST['first_name'],
+	// $uzvards = $_POST['last_name'],
+	// $epasts = $_POST['email'],
+	// $parole = $_POST['password'];
+
+
+ //  $servername = "localhost";
+ //  $username = "root";
+ //  $password = "GT5364HY";
+ //  $dbname = "rdk_db";
+
+  // Create connection
+  // $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // $user = htmlentities(mysql_real_escape_string($vards));
+  // mysql_query(INSERT INTO users(vards) VALUES('$vards') echo "Submitted.");
+  // $user = htmlentities(mysql_real_escape_string($uzvards));
+  // mysql_query(INSERT INTO users(uzvards) VALUES('$uzvards') echo "Submitted.");
+  // $user = htmlentities(mysql_real_escape_string($vards));
+  // mysql_query(INSERT INTO users(epasts) VALUES('$epasts') echo "Submitted.");
+// }
+
+//  -->
