fd98303b572920ca7465a63273bc66e6ebf02a8a
Mairis Januns <mairisja@administrators-MacBook-Pro-9.local>
2018-06-07 18:07:48 +0300

    DB_CONFIG chabnges

--- register.php
+++ register.php
@@ -115,7 +115,7 @@
         <div class="container"> <!-- col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4"> -->
           <div class="panel panel-default">
             <div class="panel-body">
-              <form action="db_config.php" method="post" role="centered-form">
+              <form action="db_config.php" method="get" role="centered-form">
                 <div class="row">
                   <div class="col-xs-6 col-sm-6 col-md-6">
                     <div class="form-group">
@@ -146,7 +146,7 @@
                   </div>
                 </div>
                 
-                <input type="submit" value="Reģistrēties" class="btn btn-info btn-block">
+                <input type="submit" value="post" class="btn btn-info btn-block">
               
               </form>
             </div>
