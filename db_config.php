485e7caa415459a4cfcc9eda4010d8bb22ed55d5
Mairis Januns <mairisja@administrators-MacBook-Pro-9.local>
2018-06-08 15:46:53 +0300

    Fix

--- db_config.php
+++ db_config.php
@@ -16,7 +16,7 @@ $last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 $password = mysqli_real_escape_string($link, password_hash($_REQUEST['password'],PASSWORD_DEFAULT));
 
-$sql_check = "SELECT * FROM users WHERE epasts=$email";
+$sql_check = "SELECT * FROM users WHERE epasts='$email'";
 echo "$sql_check";
 $result_email = mysqli_query($link,$sql_check);
 echo "$result_email";
