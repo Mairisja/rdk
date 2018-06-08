402928eda287144d4caa171238f49742d904587b
Mairis Januns <mairisja@administrators-MacBook-Pro-9.local>
2018-06-08 11:43:43 +0300

    Fix

--- db_config.php
+++ db_config.php
@@ -16,7 +16,7 @@ $last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
  
 // attempt insert query execution
-$sql = "INSERT INTO users (first_name, last_name, email) VALUES ('$first_name', '$last_name', '$email')";
+$sql = "INSERT INTO users (vards, uzvards, epasts) VALUES ('$first_name', '$last_name', '$email')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
 } else{
