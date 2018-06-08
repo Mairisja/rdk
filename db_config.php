6225260d5e1cc368558e0cc97cf5fbda670fe4be
Mairis Januns <mairisja@administrators-MacBook-Pro-9.local>
2018-06-08 15:56:53 +0300

    Fix

--- db_config.php
+++ db_config.php
@@ -17,6 +17,8 @@ $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 $password = mysqli_real_escape_string($link, password_hash($_REQUEST['password'],PASSWORD_DEFAULT));
 
 
+if ($email = mysqli_query($link,"SELECT * FROM users WHERE epasts='$email")){
+
 $sql = "INSERT INTO users (vards, uzvards, epasts, parole) VALUES ('$first_name', '$last_name', '$email','$password')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
@@ -24,6 +26,7 @@ if(mysqli_query($link, $sql)){
 } else{
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
     echo "kk4";
+}
 }
  
 // close connection
