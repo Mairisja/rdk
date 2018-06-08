33aee034a4cc29996bad22e2acca5e749a004ad7
Mairis Januns <mairisja@administrators-MacBook-Pro-9.local>
2018-06-08 14:57:26 +0300

    Fix

--- db_config.php
+++ db_config.php
@@ -16,16 +16,20 @@ $last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 $password = mysqli_real_escape_string($link, password_hash($_REQUEST['password'],PASSWORD_DEFAULT));
 
-
-$sql = "INSERT INTO users (vards, uzvards, epasts, parole) VALUES ('$first_name', '$last_name', '$email','$password')";
-if(mysqli_query($link, $sql)){
-    echo "Records added successfully.";
-    echo "kk3";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-    echo "kk4";
+$sql_check = "SELECT * FROM users WHERE epasts='$email'";
+	if (mysql_num_rows($link, $sql_check)> 0) {
+		echo "Lietotājs ar šādu epastu jau ir reģistrēts!";
+	}
+	else{
+		$sql = "INSERT INTO users (vards, uzvards, epasts, parole) VALUES ('$first_name', '$last_name', '$email','$password')";
+		if(mysqli_query($link, $sql)){
+		    echo "Records added successfully.";
+		    echo "kk3";
+		} else{
+		    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+		    echo "kk4";
+		}
 }
-
  
 // close connection
 mysqli_close($link);
