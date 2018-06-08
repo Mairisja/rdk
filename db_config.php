227373bfeef4ecfcf5dc4effe1f31a13ec877665
Mairis Januns <mairisja@administrators-MacBook-Pro-9.local>
2018-06-08 16:08:41 +0300

    Fix

--- db_config.php
+++ db_config.php
@@ -1,26 +1,34 @@
-<!-- __________________________________________CONNECTION TO SQL __________________________________________________________________________ -->
-
 <?php
-
+//==============================================//    CONNECTION TO DATABSE INITIALISATION //================================================
 $link = mysqli_connect("localhost", "root", "GT5364HY", "rdk_db");
  
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+//==============================================//    END OF CONNECTION TO DATABSE INITIALISATION //================================================
 
 
 
-// Escape user inputs for security
+//==============================================//    ESCAPE USER INPUTS FOR SECURITY REASONS //================================================
+
 $first_name = mysqli_real_escape_string($link, $_REQUEST['first_name']);
 $last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 $password = mysqli_real_escape_string($link, password_hash($_REQUEST['password'],PASSWORD_DEFAULT));
 
+//==============================================//    END OF ESCAPE USER INPUTS CHECK //================================================
 
-if ($email = mysqli_query($link,"SELECT * FROM users WHERE epasts='$email")){
+
+
+//==============================================//    CHECK DB FOR EXISTING MAIL //================================================
+if ($email != mysqli_query($link,"SELECT * FROM users WHERE epasts='$email")){
 	echo "Fuck, jau aizņemts!";
 	eader('Location: services.php'); 
 }
+//==============================================//    END OF CHECK DB FOR EXISTING MAIL //================================================
+
+
+
 else{
 $sql = "INSERT INTO users (vards, uzvards, epasts, parole) VALUES ('$first_name', '$last_name', '$email','$password')";
 if(mysqli_query($link, $sql)){
